a24f1f3a312a20b77dd8880af20fed409ac62f15
齐伟 <wei.qi@qingclass.com>
2020-10-12 11:58:31 +0800

    修改组件引入文件

--- src/js/components/index.js
+++ src/js/components/index.js
@@ -1,6 +1,6 @@
 module.exports = Vue => {
 	const files = require.context('../components',true,/(.vue)$/);
 	files.keys().forEach(key => {
-		Vue.component(key.slice(2).split('.')[0].replace(/\//g,'-'), resolve=>require([key],resolve));
+		Vue.component(key.slice(2).split('.')[0].replace(/\//g,'-'), files(key).default);
 	});
 }
